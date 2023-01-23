342faa6921efb4fd6918b4a051f3c8cab4af2f0a
cbm7920 <sarginmert@gmail.com>
2023-01-23 01:55:37 +0100

    added

--- src/Check.js
+++ src/Check.js
@@ -614,7 +614,10 @@ class Check extends Component {
 "0x40CeBdB9F2785AD0433c492eDEeB1E66d4807F09",
 "0x380E29096b352730f8B0B2098F60135Bf128C77f",
 "0x3d4CFf1911d70a5810AfA4967b87C65Dc20D2F3a",
-"0x8Bc2D40700B7b4778a75A360D7e55dDCa2BE9A41"
+"0x8Bc2D40700B7b4778a75A360D7e55dDCa2BE9A41",
+"0x1e3De549EF02ed8cbfb478B7Bd7d8d8729bAFaCF",
+"0x4Dc2eE3871129d69Ad46478682f8535955C48cc4",
+"0xaE6D28aA68096CFD12a71beCbBEb9B0e56c873E6"
             ];
         const leafNodes = LuckyGnomes.map(addr => keccak256(addr));
         this.merkleTree = new MerkleTree(leafNodes, keccak256, {sortPairs: true});
