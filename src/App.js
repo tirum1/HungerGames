a57b2cb34a7546b82644b0950c6088dc8536e0e5
cbm7920 <sarginmert@gmail.com>
2023-01-26 15:01:31 +0100

    added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import styled from "styled-components";
 import { useState, useRef } from "react";
 import "./App.css";
+import DiscordLogo from './assets/social-media-icons/discord2.png';
+import TwitterLogo from './assets/social-media-icons/twitter_32x32.png';
+import HeyMintLogo from './assets/social-media-icons/heymint.png';
 import Check from "./Check";
 import LandingPage from "./LandingPage";
 import { Flex } from "@chakra-ui/react";
 import { Howl } from "howler";
 import Creatures from "./assets/sound/1.mp3";
+import HoverSound from "./assets/sound/click-21156.mp3";
 import { useEffect } from "react";
 import clickSound from "./assets/sound/hover.mp3";
+
+const TextElement = styled(Text)`
+  font-size: 38px;
+  text-shadow: 0 10px #000000;
+
+  &:hover{
+    color:red;
+ }
+
+  @media (max-width: 50vw) {
+    font-size: 24px;
+  }
+`;
 const ButtonElement = styled.button`
 background-color: #D6517D;
 border-radius: 5px;
@@ -29,13 +46,19 @@ function App() {
   const [buttonText, setButtonText] = useState("Music");
   const [showCheck, setShowCheck] = useState(false);
   const [currentComponent, setCurrentComponent] = useState("Check");
+  const [shadow, setShadow] = useState("#FF000000");
   const [isPlaying, setIsPlaying] = useState(false);
+  const [shake, setShake] = useState(false);
   const soundRef = useRef(new Howl({
     src: Creatures,
     loop: true
   }));
   const clickSoundRef = useRef(new Howl({
     src: clickSound,
+    loop: false
+  }));
+  const hoverSoundRef = useRef(new Howl({
+    src: HoverSound,
     loop: false
   }));
   useEffect(() => {
@@ -46,7 +69,6 @@ function App() {
 
   const SoundPause = () => {
     clickSoundRef.current.play();
-    console.log("clicked");
     if (isPlaying) {
       soundRef.current.stop();
       setIsPlaying(false);
@@ -55,17 +77,56 @@ function App() {
       setIsPlaying(true);
     }
   };
+  function handleMouseEnter() {
+    setShake(true);
+    hoverSoundRef.current.play();
+    setShadow("#FFFF00");
+  }
+
+  function handleMouseLeave() {
+    setShake(false);
+    setShadow("#FF000000");
+  }
 
   return (
     <div>
       <div className="App">
       <Flex justify="flex-end" align="center" width="18%" padding="1%">
-      <div className="button-container">
+      <div
+      className={`logo-container ${shake ? 'shake' : ''}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+   <a href="https://discord.gg/wkRaMTRaZA" target="_blank">
+  <img src={DiscordLogo} alt="Join our Discord server" width="50px" height="50px" style={{marginRight: "15px"}}/>
+</a>
+    </div>
+    <div
+      className={`Twitter-logo-container ${shake ? 'shake' : ''}`}
+      onMouseEnter={() => { handleMouseEnter() }}
+      onMouseLeave={handleMouseLeave}
+    >
+      <a href="https://twitter.com/HungerGamesNFT" target="_blank">
+      <img src={TwitterLogo} alt="Join our Discord server" width="50px" height="50px" style={{marginRight: "15px"}}/>
+      </a>
+    </div>
+    <div
+      className={`HeyMint-logo-container ${shake ? 'shake' : ''}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <a href="https://heymint.xyz/luckygnomes" target="_blank">
+      <img src={HeyMintLogo} alt="Join our Discord server" width="50px" height="50px" style={{marginRight: "15px"}}/>
+</a>
+    </div>
+<div className="button-container">
+
+
   <ButtonElement
     className="shake"
     type="submit"
     onClick={SoundPause}
-    onMouseEnter={() => setButtonText("Music")}
+    onMouseEnter={() => { setButtonText("Music"); hoverSoundRef.current.play(); }}
     onMouseLeave={() => setButtonText(isPlaying ? "Stop" : "Play")}
   >
     {buttonText}
@@ -76,6 +137,7 @@ function App() {
           <LandingPage
             onButtonClick={() => {
               clickSoundRef.current.play();
+              
               setShowCheck(true);
             }}
           />
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -2,31 +2,12 @@ import styled from 'styled-components';
 import React, { Component } from 'react';
 import"./App.css";
 import { Box, Button, Flex, Input, Text, Link} from '@chakra-ui/react';
-// import Creatures from "./assets/sound/1.mp3";
 import hover from "./assets/sound/hover.mp3";
 import {Howl,Howler} from "howler";
+import HoverSound from "./assets/sound/click-21156.mp3";
 import { Buffer } from "buffer/";
 window.Buffer = window.Buffer || Buffer;
-// const audio =[
-//     {sound: Creatures, label:'Sound'}
-// ]
-const ButtonElement = styled.button`
-  background-color: #D6517D;
-  border-radius: 5px;
-  box-shadow: 0px 2px 2px 1px #0F0F0F;
-  color: black;
-  cursor: pointer;
-  font-family: inherit;
-  padding: 5px;
-  margin: 0 15px;
-  transition: background-color 0.2s ease;
 
-  &:hover {
-    background-color: #ff5252;
-    box-shadow: 0px 2px 2px 1px #FFFF00;
-    color: white;
-  }
-`;
 const FlexContainer = styled(Flex)`
   height: 110vh;
   padding-bottom: 150px;
@@ -35,23 +16,44 @@ const FlexContainer = styled(Flex)`
     height: 80vh;
   }
 `;
-const BoxContainer = styled(Box)`
-  width: 700px;
-  
-  @media (max-width: 768px) {
-    width: 80%;
+
+const TextContainer = styled.div`
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const TextElement = styled(Text)`
+
+  text-shadow: 0 10px #000000;
+  text-align: center;
+  &:hover {
+    color: red;
   }
 `;
-const TextElement = styled(Text)`
-  font-size: 38px;
-  text-shadow: 0 10px #000000;
 
-  &:hover{
-    color:red;
- }
+const ButtonContainer = styled.div`
+position: absolute;
+top: 60%;
+left: 50%;
+transform: translate(-50%, -50%);
+`;
 
-  @media (max-width: 50vw) {
-    font-size: 24px;
+const ButtonElement = styled.button`
+background-color: #D6517D;
+border-radius: 5px;
+box-shadow: 0px 2px 2px 1px #0F0F0F;
+color: black;
+cursor: pointer;
+font-family: inherit;
+padding: calc(0.5vw + 0.5vh);
+transition: background-color 0.2s ease;
+
+  &:hover {
+    background-color: #ff5252;
+    box-shadow: 0px 2px 2px 1px #FFFF00;
+    color: white;
   }
 `;
 
@@ -59,39 +61,13 @@ class LandingPage extends Component {
     constructor(props){
         super(props);
         this.hover = new Howl({ src: hover });
+        this.hoverover = new Howl({ src: HoverSound });
         this.state = {
             result: null,
             hover: false,
         }
     }
 
-
-// RenderButtonAndSound = () => {
-//     return audio.map((soundObj, index) => {
-//         return(
-         
-//             <ButtonElement
-// className="shake"
-// marginTop="200px"
-// type="submit"
-// backgroundColor="#D6517D"
-// borderRadius="5px"
-// boxShadow="0px 2px 2px 1px #0F0F0F"
-// color="Black"
-// cursor="pointer"
-// fontFamily="inherit"
-// padding="10px"
-// margin="0 50px"
-// key={index} 
-// onClick={() => {this.SoundPause(); this.HoverPlay()}} 
-// onMouseEnter={() => this.setState({buttonText: "Music"})} 
-// onMouseLeave={() => this.setState({buttonText: this.state.isPlaying ? "Stop" : "Play"})}
-// >
-// {this.state.buttonText}
-// </ButtonElement>
-//         )
-//     })
-// }
 handleSubmit = (e) => {
     e.preventDefault();
     this.HoverPlay();
@@ -102,19 +78,20 @@ SoundPlay = () => {
   HoverPlay = () => {
   this.hover.play();
 }
-// componentDidMount() {
-//     this.SoundPlay();
-//   }
+HoverOverPlay = () => {
+    this.hoverover.play();
+  }
+
   render() {
     Howler.volume(0.15,0)
-    return ( 
+    return ( <div>
          <div>
-            
-              {/* {this.RenderButtonAndSound()} */}
-            
-            <FlexContainer justify="center" align="center">
-                <BoxContainer>
-                    <Text fontSize="38px" textShadow="0 10px #000000">What is to come?</Text>
+             <TextContainer>
+                    <TextElement fontSize="calc(2vh + 2vw)" className ="shake" textShadow="0 10px #000000">What is to come?</TextElement>
+                    </TextContainer>
+            </div>
+            <div>
+                    <ButtonContainer>
                     <ButtonElement
                         className='shake'
                         type="submit"
@@ -124,14 +101,14 @@ SoundPlay = () => {
                         color="Black"
                         cursor="pointer"
                         fontFamily="inherit"
-                        padding="5px"
-                        margin="0 15px"
+                        onMouseEnter={() => { this.HoverOverPlay() }}
                         onClick={this.props.onButtonClick}>CHECK WHITELIST
                     </ButtonElement>
-                </BoxContainer>
-            </FlexContainer>
+                    </ButtonContainer>
 
-        </div>
+         
+
+        </div></div>
     )
 }
 
