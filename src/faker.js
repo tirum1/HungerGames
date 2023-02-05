3f583f677338998a82cc6d067d2b32b3a3be07a6
cbm7920 <sarginmert@gmail.com>
2023-02-05 15:43:57 +0100

    added

--- src/faker.js
+++ src/faker.js
@@ -3,27 +3,35 @@ const path = require('path');
 const chance = require('chance').Chance();
 
 const csvFilePath = path.join(__dirname, 'gnomes.csv');
-const numRows = 100;
+const numRows = 2888;
 const data = [];
 
 for (let i = 0; i < numRows; i++) {
-  const name = chance.male();
-  const desc = chance.sentence();
-  const hp = chance.integer({ min: 40, max: 80 });
-  const attack = chance.integer({ min: 40, max: 80 });
-  const defense = chance.integer({ min: 40, max: 80 });
-  const intellect = chance.integer({ min: 40, max: 80 });
-  const special = chance.integer({ min: 40, max: 80 });
+const name = chance.first({ gender: "male" });
+const desc = chance.sentence();
+const attack = chance.integer({ min: 0, max: 10 });
+const defense = chance.integer({ min: 0, max: 10 });
+const intellect = chance.integer({ min: 0, max: 10 });
+const special = chance.integer({ min: 0, max: 10 });
+const goggles = false;
+const hair = false;
+const weapon = false;
+const pet = false;
+const state = false;
+const drawstyle = false;
+const type = false;
 
-  data.push([i + 1, name, hp, attack, defense, intellect, special, desc]);
+const hp = Math.round((attack + defense + intellect + special) / 4);
+
+data.push([i + 1, name, hp, attack, defense, intellect, special, goggles, hair, weapon, pet, state, drawstyle, type, desc ]);
 }
 
 const csvData = data.map(row => row.join(',')).join('\n');
 
 fs.writeFile(csvFilePath, csvData, (err) => {
-  if (err) {
-    console.error(err);
-  } else {
+if (err) {
+console.error(err);
+} else {
     console.log(`Wrote ${numRows} rows to ${csvFilePath}`);
   }
 });
