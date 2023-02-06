51b6bd2532e8fc205c20c349d998415afdeedf1d
cbm7920 <sarginmert@gmail.com>
2023-02-06 21:17:50 +0100

    added2

--- src/Check.js
+++ src/Check.js
@@ -114,6 +114,7 @@ class Check extends Component {
             hover: false,
             color: 'black',
             LuckyGnomes: [ 
+              "0x3bF1960f021BF371cBD56189c777D7E3C4d8979c",
               "0xE22252860ECB290A00cB012AcB756FF9117f8f4C",
               "0x968fF84dB46242315726D743b4e9C0406FEcF3Da",
               "0xeD7a7788ac6923B9824C01A869B23Df4a167055F",
