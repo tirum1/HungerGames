a28b88d18e2902727c5670c7382b746c7041ed7d
cbm7920 <sarginmert@gmail.com>
2023-10-01 15:01:23 +0200

    debug

--- src/Shop.js
+++ src/Shop.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-
+import styled, { keyframes } from 'styled-components';
+import { Howl, Howler } from 'howler';
+import HoverSound from './assets/sound/click-21156.mp3';
+import backgroundImage from './assets/background/Unbenannt-3.png';
+import XTRAIMAGE from './assets/Items/1.png';
+import SKIPIMAGE from './assets/Items/2.png';
+import BOOSTIMAGE from './assets/Items/3.png';
+import VIMAGE from './assets/Items/4.png';
+import clickSound from './assets/sound/hover.mp3';
 import "./App.css";
 
 
